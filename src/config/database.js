dee4e2b9e5106e1e6571adc3ebe3bf68741a095a
AndreCorrea <andreff55@gmail.com>
2022-08-19 15:05:08 -0300

    update database config

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,13 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
-mongoose.connect(`mongodb://127.0.0.1:27017/automanager`).then(() => {
-    console.log('Conectado ao BD')
+const dbUser = process.env.DB_USER
+const dbName = process.env.DB_NAME
+const dbPass = process.env.DB_PASS
+const dbCluster = process.env.DB_CLUSTER
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@${dbCluster}.mongodb.net/${dbName}?retryWrites=true&w=majority`).then(() => {
+    console.log('Connected database')
 }).catch((error) => {
     console.log(error)
 })
