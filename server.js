074204f2ec2726831738c1ad1c6c1d4c72526022
AndreCorrea <andreff55@gmail.com>
2022-08-19 17:40:08 -0300

    update files

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express()
 
 app.use(express.json())
 
+const portHost = 5000
+
 require('./src/config/database')
 require('./src/routes/app')(app)
 
-app.listen('5000', () => {
+app.listen(portHost, () => {
     console.log('Server is running on port 5000')
 })
